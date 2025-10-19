60cd5847b8c2d98556ee642f2b34551fc34db957
Богдан Корогодский <bogdankorogodskij@gmail.com>
2025-10-19 15:11:24 +0300

    добавил роут и разметку

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Layer } from './commonUI/layer/layer';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Layer}
+];
